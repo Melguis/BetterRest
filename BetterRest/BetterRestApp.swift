1758725fd7f2d14c99e37beadfedac1baff1b949
Jorge Henrique <jorgehenriquedasilva2014@hotmail.com>
2023-06-08 10:13:06 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jorge Henrique on 08/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
